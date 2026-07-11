67e5f307ec2574b0614cd8f1ba818dc4861938ba
Alik Green <alik.green25@students.bhasvic.ac.uk>
2026-07-11 15:11:15 +0100

    fix task 9

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
         task1: 'task1.html',
         task4: 'task4.html',
         task6: 'task6.html',
-        task8: 'task8.html'
+        task8: 'task8.html',
+        task9: 'task9.html',
       }
     }
   }

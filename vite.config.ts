9ceb6e6dcb883ef294367c7964962ed537820aa5
Alik Green <alik.green25@students.bhasvic.ac.uk>
2026-07-11 16:14:36 +0100

    added task 5 and 7

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
         task1: 'task1.html',
         task3: 'task3.html',
         task4: 'task4.html',
+        task5: 'task5.html',
         task6: 'task6.html',
+        task7: 'task7.html',
         task8: 'task8.html',
         task9: 'task9.html',
       }
